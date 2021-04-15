ae9bdd195edde9226f7ee25b7032ed7cb943a4c4
taylorlohan <939691738@qq.com>
2021-04-15 03:29:24 +0100

    tweak on library

--- mylanguage.health.library/src/mylanguage/health/library/People.java
+++ mylanguage.health.library/src/mylanguage/health/library/People.java
@@ -1,4 +1,7 @@
 package mylanguage.health.library;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class People {
     float height=0;
@@ -10,6 +13,14 @@ public class People {
     float standardWeight=0;
     float energy=0;
     String name="";
+    JFrame frameEat=new JFrame("Your nutrition advice");
+    JFrame frameSport=new JFrame("Your exercise advice");
+    JPanel panelEat = new JPanel();
+    JLabel labelEat = new JLabel("");
+    JPanel panelSport = new JPanel();
+    JLabel labelSport = new JLabel("");
+
+    
     
     public void setName(String n) {
     	this.name=n;
@@ -68,28 +79,42 @@ public class People {
     
     public void suggestionOnEating() {
     	computeStandard();
+    	panelEat.add(labelEat);
+	    frameEat.add(panelEat);
+	    frameEat.setVisible(true);
+		frameEat.setBounds(600, 300, 500, 400);
+        frameEat.setSize(600,200);
+        frameEat.setLocation(600, 200);
+        frameEat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	if((this.intakeEnergy-this.energy)>0) {
-    		System.out.println("your intake is greater than the standard nuitrition intake, you should control your diet");
+            labelEat.setText("your intake today is greater than the standard daily nuitrition intake, you should control your diet");
     	}
     	else if((this.intakeEnergy-this.energy)<0)
     	{
-    		System.out.println("your intake is smaller than the standard nuitrition intake, you should eat more");
+    		labelEat.setText("your intake today is smaller than the standard daily nuitrition intake, you should eat more");
     	}
     	else {
-    		System.out.println("your intake is equal to the standard nuitrition intake, you may not worry about your diet");
+    		labelEat.setText("your intake today is equal to the standard daily nuitrition intake, you may not worry about your diet");
     	}
     }
     
     public void suggestionOnSport() {
+    	panelSport.add(labelSport);
+	    frameSport.add(panelSport);
+	    frameSport.setVisible(true);
+		frameSport.setBounds(600, 300, 500, 400);
+        frameSport.setSize(600,200);
+        frameSport.setLocation(600, 300);
+        frameSport.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     	if((this.intakeEnergy-this.sport)>0) {
-    		System.out.println("your intake is greater than sport, you should do more exercise");
+    		labelSport.setText("your intake is greater than sport today, you should do more exercise");
     	}
     	else if((this.intakeEnergy-this.sport)<0)
     	{
-    		System.out.println("your intake is smaller than sport, you should pay your attention more to your diet");
+    		labelSport.setText("your intake is smaller than sport today, you should pay more attention to your diet");
     	}
     	else {
-    		System.out.println("your intake is equal to sport, you shouldn't worry much about your health");
+    		labelSport.setText("your intake is equal to sport today, you shouldn't worry much about your health");
     	}
     }
    
